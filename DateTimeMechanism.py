037c355d748f505da9d1171c949bbb5aa06bda89
Vaibhav Vasdev <vaibhavvasdev63@gmail.com>
2023-08-06 22:12:05 +0530

    Update DateTimeMechanism.py

--- DateTimeMechanism.py
+++ DateTimeMechanism.py
@@ -2,13 +2,19 @@ import requests
 import datetime
 import dateutil.parser
 from dateutil import tz
+import tkinter as tk
+from tkinter import simpledialog
 
-# Enter location Ids
-location_ID = ["",""] #Enter the OpenAQ Location ID's here
+# Create a Tkinter root window
+root = tk.Tk()
+root.withdraw()  # Hide the main window
 
-# Enter your OpenAQ API key
-api_Key = " "
-api_key_1 = api_Key.strip()
+# Get user input for location IDs
+location_ids_input = simpledialog.askstring("Input", "Enter the OpenAQ Location IDs (separated by commas): ")
+location_ID = location_ids_input.split(",")
+
+# Get user input for OpenAQ API key
+api_key = simpledialog.askstring("Input", "Enter your OpenAQ API key: ").strip()
 
 # Initialize lists to store longitude and latitude for each location
 longitudes = []
@@ -22,7 +28,7 @@ for location_id in location_ID:
     url = f"https://api.openaq.org/v2/latest?location_id={location_id}&limit=100"
 
     # Set the Authorization header with the API key
-    headers = {"X-API-Key": api_key_1}
+    headers = {"X-API-Key": api_key}
 
     response = requests.get(url, headers=headers)
 
@@ -71,10 +77,13 @@ if all_dates:
 else:
     print("No dates found.")
 
+# Get user input for OpenWeatherMap API key
+api_key_weather = simpledialog.askstring("Input", "Enter your OpenWeatherMap API key: ")
+
 # Below code is for OpenWeatherMapsAPI's date and time
 
 def get_city_name(latitude, longitude, api_key):
-    url = f'http://api.openweathermap.org/geo/1.0/reverse?lat={latitude}&lon={longitude}&appid={api_key}'
+    url = f'http://api.openweathermap.org/geo/1.0/reverse?lat={latitude}&lon={longitude}&appid={api_key_weather}'
     response = requests.get(url)
 
     if response.status_code == 200:
@@ -85,13 +94,13 @@ def get_city_name(latitude, longitude, api_key):
         print(f"Failed to retrieve city name for coordinates ({latitude}, {longitude}).")
         return None
 
-def get_weather_data(latitude, longitude, api_key):
+def get_weather_data(latitude, longitude, api_key_weather):
     # Get city name based on latitude and longitude
     city_name = get_city_name(latitude, longitude, api_key)
 
     if city_name:
         # API endpoint for weather data
-        url = f'http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}'
+        url = f'http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key_weather}'
 
         # Send request to the API
         response = requests.get(url)
@@ -108,33 +117,34 @@ def get_weather_data(latitude, longitude, api_key):
         print(f"Failed to retrieve city name for coordinates ({latitude}, {longitude}).")
         return None
 
-# Example usage
-api_key = " "  # Replace with your OpenWeatherMap API key
+# Process weather data for each location
+for i in range(len(location_ID)):
+    latitude = latitudes[i]
+    longitude = longitudes[i]
 
-# Get weather data based on latitude and longitude
-weather_data = get_weather_data(latitudes[0], longitudes[0], api_key)
+    # Get weather data based on latitude and longitude
+    weather_data = get_weather_data(latitude, longitude, api_key_weather)
 
-# Process the weather data as needed
-if weather_data:
-    # Extract the relevant information from the data dictionary
-    wind = weather_data['wind']
-    wind_speed = wind['speed']
-    wind_direction = wind['deg']
-    timestamp = weather_data['dt']  # Extract the Unix timestamp
+    # Process the weather data as needed
+    if weather_data:
+        # Extract the relevant information from the data dictionary
+        wind = weather_data['wind']
+        wind_speed = wind['speed']
+        wind_direction = wind['deg']
+        timestamp = weather_data['dt']  # Extract the Unix timestamp
 
-    # Convert the timestamp to ISO 8601 format
-    def convert_to_iso8601(date_str):
-        parsed_date = dateutil.parser.parse(date_str)
-        iso8601_date = parsed_date.astimezone(tz.tzutc()).isoformat()
-        return iso8601_date
-
-    if timestamp:
         # Convert the timestamp to ISO 8601 format
-        iso8601_date = convert_to_iso8601(datetime.datetime.fromtimestamp(timestamp).strftime("%Y-%m-%d %H:%M:%S"))
+        def convert_to_iso8601(date_str):
+            parsed_date = dateutil.parser.parse(date_str)
+            iso8601_date = parsed_date.astimezone(tz.tzutc()).isoformat()
+            return iso8601_date
 
-        print(f"Date: {iso8601_date}")
+        if timestamp:
+            # Convert the timestamp to ISO 8601 format
+            iso8601_date = convert_to_iso8601(datetime.datetime.fromtimestamp(timestamp).strftime("%Y-%m-%d %H:%M:%S"))
+
+            print(f"Date for Location ID '{location_ID[i]}': {iso8601_date}")
+        else:
+            print(f"Date for Location ID '{location_ID[i]}': Not available")
     else:
-        print("Date: Not available")
-else:
-    print("Failed to retrieve weather data. Please check your API key or network connection.")
-
+        print(f"Failed to retrieve weather data for Location ID '{location_ID[i]}'. Please check your API key or network connection.")
