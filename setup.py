ee73abbf3d2a2901665b1ef82d697ff99fcc549a
Vaibhav Vasdev <vaibhavvasdev63@gmail.com>
2024-07-04 02:30:18 +0530

    Update setup.py
    "long_description_content_type" set to markdown

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version='0.0.6',
     description='The Gaussian Plume Model for atmospheric dispersion and inverse modeling of contaminants with support for multiple sources and receptors.',
     long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     url='https://github.com/VaibhavVasdev/Gaussian-Plume_Model',  
     author='Vaibhav Vasdev',
     author_email='vaibhavvasdev63@gmail.com',
