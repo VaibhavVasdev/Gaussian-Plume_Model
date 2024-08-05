1f568e0a1863cfbdfa37391cfa90756466ffd7b1
Vaibhav Vasdev <vaibhavvasdev63@gmail.com>
2024-08-06 00:35:18 +0530

    Version corrected in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gplume',
-    version='0.0.7',
+    version='1.0.0',
     description='The Gaussian Plume Model for atmospheric dispersion and inverse modeling of contaminants with support for multiple sources and receptors.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
